587439505091bb7f1e95cf8f02ca306c50658bd2
Yaroslav Kravets <yar.kravets@ukr.net>
2021-05-22 03:05:10 +0300

    update task 2

---
+++ events/1/js/script.js
@@ -0,0 +1,17 @@
+let acc = document.querySelectorAll(".accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+let allparagraff = document.querySelectorAll("p");
+//for
